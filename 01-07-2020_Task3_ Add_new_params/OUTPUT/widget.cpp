16e79c15f3cdb1992fd267fb52af9b3f7fa4aa3d
Andrew-M30-321B-17 <67622926+Andrew-M30-321B-17@users.noreply.github.com>
2020-07-09 19:12:29 +0600

    Add files via upload

---
+++ 01-07-2020_Task3_ Add_new_params/OUTPUT/widget.cpp
@@ -0,0 +1,47 @@
+#include "getvisualfromch.h"
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    connect(ui->pushButton, SIGNAL(clicked()),this, SLOT(pBClicked()));
+    connect(ui->pushButton_2, SIGNAL(clicked()),this, SLOT(pB2Clicked()));
+    connect(ui->pushButton_3, SIGNAL(clicked()),this, SLOT(pB3Clicked()));
+    connect(ui->pushButton_4, SIGNAL(clicked()),this, SLOT(pB4Clicked()));
+    connect(ui->pushButton_5, SIGNAL(clicked()),this, SLOT(pB5Clicked()));
+
+
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::pBClicked(){
+    GetVisualFromCH Car(ui);
+    Car.charts("speed","blue",ui->widget,0);
+    Car.charts("refueling","red",ui->widget,1);
+}
+void Widget::pB2Clicked(){
+    GetVisualFromCH Car(ui);
+    Car.charts("acceleration","blue",ui->widget,0);
+    Car.charts("refueling","red",ui->widget,1);
+}
+void Widget::pB3Clicked(){
+    GetVisualFromCH Car(ui);
+    Car.charts("lengthWay","blue",ui->widget,0);
+    Car.charts("refueling","red",ui->widget,1);
+}
+void Widget::pB4Clicked(){
+    GetVisualFromCH Car(ui);
+    Car.charts("fuel","blue",ui->widget,0);
+    Car.charts("refueling","red",ui->widget,1);
+}
+void Widget::pB5Clicked(){
+    GetVisualFromCH Car(ui);
+    Car.charts("Simple","blue",ui->widget,0);
+
+}
+
+
---
+++ 01-07-2020_Task3_ Add_new_params/OUTPUT/widget.h
@@ -0,0 +1,27 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+private slots:
+    void pBClicked();
+    void pB2Clicked();
+    void pB3Clicked();
+    void pB4Clicked();
+    void pB5Clicked();
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+
+private:
+    Ui::Widget *ui;
+};
+
+#endif // WIDGET_H
